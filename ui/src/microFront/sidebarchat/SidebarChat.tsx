3bc1a56e1aa78d61717e6efaa6afb375dcc5cfb8
Julio Bandeira <54188873+JulioRy@users.noreply.github.com>
2025-04-26 03:46:55 -0300

    Fixing details

---
+++ ui/src/microFront/sidebarchat/SidebarChat.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './SidebarChat.css';
+import logoJuliusIA from '../../assets/julius.png';
+import { VscSend } from "react-icons/vsc";
+
+export const SidebarChat: React.FC = () => (
+    <div className='sidebarchat-column'>
+        <div className='sidebarchat-header'>
+            <img src={logoJuliusIA} className='sidebarchat-logo' alt='logo' />
+        </div>
+        <div className='sidebarchat-content'>
+            <p className='sidebarchat-label'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p className='sidebarchat-label'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p className='sidebarchat-label'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+        <div className='sidebarchat-footer'>
+            <textarea className='sidebarchat-textarea' placeholder='Type your message...'></textarea>
+            <button className='sendButton'>
+                <VscSend size={18} />
+            </button>
+        </div>
+    </div>
+);
