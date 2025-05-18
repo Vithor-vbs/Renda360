97e524be8c339abb085a0027c640055b228814ba
Vithor-vbs <vithorvilas12@gmail.com>
2025-05-18 01:13:37 -0300

    Refactor routing paths and improve component imports; general styles improvements for better responsiveness

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,14 +1,14 @@
 // ui/src/App.tsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import { Home } from './microFront/home';
-import TransactionsPage from './microFront/transactions/TransactionsPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Home } from "./microFront/home";
+import TransactionsPage from "./microFront/transactions/TransactionsPage";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/Home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/transactions" element={<TransactionsPage />} />
       </Routes>
     </Router>
--- ui/src/microFront/mainboard/Mainboard.tsx
+++ ui/src/microFront/mainboard/Mainboard.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { DashboardHeader } from '../dashboardheader/DashboardHeader'
-import { BalancesRow } from '../balancesrow/BalancesRow'
-import './MainBoard.css'
-import { PieChartComponent } from '@/components/PieChartComponent'
+import React from "react";
+import { DashboardHeader } from "../dashboardheader/DashboardHeader";
+import { BalancesRow } from "../balancesrow/BalancesRow";
+import "./Mainboard.css";
+import { PieChartComponent } from "@/components/PieChartComponent";
 // import { LineGraph } from '@/components/LineGraph'
 
 export const MainBoard: React.FC = () => (
@@ -13,4 +13,4 @@ export const MainBoard: React.FC = () => (
     {/* <LineGraph /> */}
     {/* outros blocos virão aqui */}
   </div>
-)
+);
